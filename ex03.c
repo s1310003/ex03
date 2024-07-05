5833867c360aab11218efcbad39fee46bd0a1ecd
s1310003 <s1310003@u-aizu.ac.jp>
2024-07-05 14:28:04 +0900

    Hot: initial revision

---
+++ ex03.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(0));  
+
+    int rounds = 3;
+    int headsCount = 0;
+    int tailsCount = 0;
+
+    printf("Tossing a coin...\n");
+    for (int round = 1; round <= rounds; round++) {
+        int toss = rand() % 2; 
+        if (toss == 0) {
+            headsCount++;
+            printf("Round %d: Heads\n", round);
+        } else {
+            tailsCount++;
+            printf("Round %d: Tails\n", round);
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n", headsCount, tailsCount);
+
+    return 0;
+}
